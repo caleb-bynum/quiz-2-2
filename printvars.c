583abbe3b78abeb1b31c3f92e1f9de5e2ba70444
Caleb <caleb.bynum@tamu.edu>
2023-03-23 17:10:16 -0700

    completed

--- printvars.c
+++ printvars.c
@@ -8,6 +8,34 @@ char *custom_env[] = {"USER=Pikachu", "PATH=/tmp", NULL};
 
 int main(void)
 {
-    // TODO
+
+    printf("Running parent process PID: %d\n\n", getpid());
+
+    // fork the first child
+    pid_t child1 = fork();
+
+    if (child1 == 0) {
+        printf("Running child process PID: %d\n", getpid());
+        execle("echoall", "echoall", "Bandicoot", "Pacman", NULL, custom_env);
+        // terminate
+        exit(1);
+    }
+
+    // wait for child 1 to terminate
+    waitpid(child1, NULL, 0);
+
+    // fork the second child
+    pid_t child2 = fork();
+
+    if (child2 == 0) {
+        printf("Running child process PID: %d\n", getpid());
+        execlp("echoall", "echoall", "Spyro", NULL);
+        // terminate
+        exit(1);
+    }
+
+    // wait for child 2 to terminate
+    waitpid(child2, NULL, 0);
+
     exit(0);
 }
